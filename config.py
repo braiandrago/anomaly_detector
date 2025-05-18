4d22f99fe61d6ba63e2d50f68a8a2f4890fd081e
braiandrago <braiandrago@gmail.com>
2025-05-18 17:13:22 -0300

    archivo de configuración de entrenamiento, inferencia y simulación

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Longitud de cada secuencia de entrada (minutos / registros)
+SEQ_LENGTH = 20
+
+# Cantidad de secuencias que se simulan al consultar la API
+N_SECUENCIAS = 10
+
+# Variables que se monitorean en el caso multivariado
+MULTIVAR_FEATURES = ["temperature", "pressure", "flowrate"]
